c53e714af2871a47e3b734a74e39b2a5ab4b4b81
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-04 08:51:32 -0700

    wip

---
+++ projectRoutes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const Project = require('./data/helpers/projectModel');
+
+const router = express.Router();
--- server.js
+++ server.js
@@ -4,10 +4,26 @@ const server = express();
 
 server.use(express.json());
 
+const projectRoute = require('./projectRoutes');
+
+server.use(logger);
+server.use('/api/projects', projectRoute)
 //Test its running
 
 server.get('/', (req, res) => {
     res.status(200).json("Successful Deployment")
 })
+
+server.use(errorHandler);
+
+function logger(req, res, next) {
+    console.log(`${req.method} to ${req.path} at ${new Date().toISOString()}`)
+    next()
+};
+
+function errorHandler(err, req, res, next) {
+    console.log(err)
+}
+
 
 module.exports = server;
