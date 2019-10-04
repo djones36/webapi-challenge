ac78e6c27a61a224e065430061614e42bf5a296d
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-04 10:00:17 -0700

    wip

--- projectRoutes.js
+++ projectRoutes.js
@@ -44,6 +44,21 @@ router.post('/', validatProject, (req, res) => {
         })
 })
 
+//Put
+
+router.put('/:id', validateProjectId, (req, res) => {
+    const id = req.params.id
+    const update = req.body
+
+    Project.update(id, update).then(updatedProject => {
+        res.status(200).json(updatedProject)
+    }).catch(err => {
+        res.status(500).json({ message: "failed to update", error: err })
+    })
+})
+
+//Delete
+
 
 
 //middleware
